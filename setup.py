74fc5ac4b282ea146543d8d1eb383d529edc707b
johann2357 <johann2357@gmail.com>
2014-06-17 10:59:26 -0500

    adding setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='django-charts',
+    version='1.0',
+    description='Django Charts',
+    author='django',
+    url='https://github.com/johann2357/django-charts',
+    packages=['django-charts', ],
+)
